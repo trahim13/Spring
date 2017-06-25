185cde4d3b869bcdd58b2a7f4dc3d695d90c28c9
Yauheni Trakhimovich <trachimovich@gmail.com>
2017-06-25 22:58:28 +0300

    Analysis methods work speed and print same resalt MAP and Set

--- src/main/java/spring/aop/logging/MyLogger.java
+++ src/main/java/spring/aop/logging/MyLogger.java
@@ -3,6 +3,9 @@ package spring.aop.logging;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.Set;
+
 @Component
 public class MyLogger {
     public Object whatTime(ProceedingJoinPoint joinPoint) {
@@ -21,6 +24,25 @@ public class MyLogger {
         System.out.println("method end: " + joinPoint.getSignature().toShortString() + " ,time= " + time + " ms");
 
         return output;
+    }
+
+    public void print(Object obj) {
+        System.out.println("Print info begin>>");
+
+        if (obj instanceof Set) {
+            Set set = (Set) obj;
+            for (Object object : set) {
+                System.out.println(object);
+            }
+        } else if (obj instanceof Map) {
+            Map map = (Map) obj;
+            for (Object object : map.keySet()) {
+                System.out.println("key=" + object + ", " + map.get(object));
+            }
+        }
+
+        System.out.println("Print info end<<");
+        System.out.println();
     }
 
 }
